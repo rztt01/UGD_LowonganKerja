f4b7eeead314401202062d0743de84e572cd61a5
rztt01* <vinsariest@gmail.com>
2022-09-06 15:13:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lat_ugd1/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.lat_ugd1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    // Atribute yang akan kita pakai
+    private lateinit var inputUserName: TextInputLayout
+    private lateinit var inputPassword: TextInputLayout
+    private lateinit var mainLayout: ConstraintLayout
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Ubah Title pada App Bar Aplikasi
+        setTitle("User Login")
+
+        // Hubungkan variabel dengan view di layoutnya.
+        inputUserName = findViewById(R.id.inputLayoutUsername)
+        inputPassword = findViewById(R.id.inputLayoutPassword)
+        mainLayout = findViewById(R.id.mainLayout)
+        val btnRegister: Button = findViewById(R.id.btnRegister)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+
+        // Aksi pada btnLogin
+        btnLogin.setOnClickListener(View.OnClickListener {
+            var checkLogin = false
+            val username: String = inputUserName.getEditText()?.getText().toString()
+            val password: String = inputPassword.getEditText()?.getText().toString()
+
+            // Pengecekan apakah inputan kosong
+            if (username.isEmpty()) {
+                inputUserName.setError("Username must be filled with text")
+                checkLogin = false
+            }
+
+            // Pengecekan apakah inputan kosong
+            if (password.isEmpty()) {
+                inputPassword.setError("Password must be filled with text")
+                checkLogin = false
+            }
+
+            // Ganti Password dengan NPM kalian.
+            if (username == "admin" && password == "0724") {
+                inputUserName.getEditText()?.setText("")
+                inputPassword.getEditText()?.setText("")
+                checkLogin = true
+                Snackbar.make(mainLayout, "Login Successful!", Snackbar.LENGTH_LONG).show()
+            }
+
+        })
+
+        // Aksi btnRgister ketika di klik
+        btnRegister.setOnClickListener(View.OnClickListener {
+            var checkRegister = false
+            val username: String = inputUserName.getEditText()?.getText().toString()
+            val password: String = inputPassword.getEditText()?.getText().toString()
+
+            // Pengecekan apakah inputan kosong
+            if (username.isEmpty()) {
+                checkRegister = true
+            }
+
+            // Pengecekan apakah inputan kosong
+            if (password.isEmpty()) {
+                checkRegister = true
+            }
+
+            if (!checkRegister) return@OnClickListener
+            val moveHome = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(moveHome)
+        })
+
+        }
+    }
---
+++ app/src/main/java/com/example/lat_ugd1/RegisterActivity.kt
@@ -0,0 +1,71 @@
+package com.example.lat_ugd1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+
+class RegisterActivity : AppCompatActivity() {
+    // Atribute yang akan kita pakai
+    private lateinit var inputUserNameRegister: TextInputLayout
+    private lateinit var inputPasswordRegister: TextInputLayout
+    private lateinit var inputConfirmPasswordRegister: TextInputLayout
+    private lateinit var inputEmailRegister: TextInputLayout
+    private lateinit var mainLayout2: ConstraintLayout
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        // Ubah Title pada App Bar Aplikasi
+        setTitle("User Register")
+
+        // Hubungkan variabel dengan view di layoutnya.
+        inputUserNameRegister = findViewById(R.id.inputLayoutUsername2)
+        inputPasswordRegister = findViewById(R.id.inputLayoutPassword2)
+        inputConfirmPasswordRegister = findViewById(R.id.inputLayoutConfirmPassword)
+        inputEmailRegister = findViewById(R.id.inputLayoutEmail)
+        mainLayout2 = findViewById(R.id.mainLayout2)
+        val btnRegister2: Button = findViewById(R.id.btnRegister2)
+
+        // Aksi pada btnLogin
+        btnRegister2.setOnClickListener(View.OnClickListener {
+            var checkRegister = false
+            val username: String = inputUserNameRegister.getEditText()?.getText().toString()
+            val password: String = inputPasswordRegister.getEditText()?.getText().toString()
+            val confirm: String = inputConfirmPasswordRegister.getEditText()?.getText().toString()
+            val email: String = inputEmailRegister.getEditText()?.getText().toString()
+
+            // Pengecekan apakah inputan kosong
+            if (username.isEmpty()) {
+                inputUserNameRegister.setError("Username must be filled with text")
+                checkRegister = false
+            }
+
+            // Pengecekan apakah inputan kosong
+            if (password.isEmpty()) {
+                inputPasswordRegister.setError("Password must be filled with text")
+                checkRegister = false
+            // Pengecekan apakah input confirm password sama dengan password
+            } else if (!password.equals(confirm)) {
+            inputConfirmPasswordRegister.setError("Password would not be matched")
+            checkRegister = false
+            }
+
+            // Pengecekan apakah inputan kosong
+            if (email.isEmpty()) {
+                inputEmailRegister.setError("Email must be filled with text")
+                checkRegister = false
+            }
+
+            if (!checkRegister) return@OnClickListener
+            val moveHome = Intent(this@RegisterActivity, MainActivity::class.java)
+            startActivity(moveHome)
+        })
+    }
+}
