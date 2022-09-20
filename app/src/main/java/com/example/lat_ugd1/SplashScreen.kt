31984c4dec4b016dd634ba898b37b6b99f6d8146
rztt01* <vinsariest@gmail.com>
2022-09-20 21:43:09 +0700

    nyoba splashscreen (2)

--- app/src/main/java/com/example/lat_ugd1/SplashScreen.kt
+++ app/src/main/java/com/example/lat_ugd1/SplashScreen.kt
@@ -1,26 +1,38 @@
 package com.example.lat_ugd1
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Handler
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
-@Suppress("DEPRECATION")
+
 class SplashScreen : AppCompatActivity() {
+    
+    var prevStarted = "yes"
+    override fun onResume() {
+        super.onResume()
+        val isTrue: Boolean = true
+        val isFalse: Boolean = false
+        val sharedpreferences: SharedPreferences
+
+        sharedpreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
+        if (!sharedpreferences.getBoolean(prevStarted, isFalse)) {
+            val editor = sharedpreferences.edit()
+            editor.putBoolean(prevStarted, isTrue)
+            editor.apply()
+        } else {
+            moveToSecondary()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_main)
+    }
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+    fun moveToSecondary() {
+        // use an intent to travel from one activity to another.
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
