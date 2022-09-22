855c5675316b2c5002685f3ace796f8fd03e6709
rztt01* <vinsariest@gmail.com>
2022-09-22 19:45:16 +0700

    splash screen fix

--- app/src/main/java/com/example/lat_ugd1/SplashScreen.kt
+++ app/src/main/java/com/example/lat_ugd1/SplashScreen.kt
@@ -1,38 +1,27 @@
 package com.example.lat_ugd1
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 
+@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
-
-    var prevStarted = "yes"
-    override fun onResume() {
-        super.onResume()
-        val isTrue: Boolean = true
-        val isFalse: Boolean = false
-        val sharedpreferences: SharedPreferences
-
-        sharedpreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
-        if (!sharedpreferences.getBoolean(prevStarted, isFalse)) {
-            val editor = sharedpreferences.edit()
-            editor.putBoolean(prevStarted, isTrue)
-            editor.apply()
-        } else {
-            moveToSecondary()
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        setContentView(R.layout.activity_splash_screen)
 
-    fun moveToSecondary() {
-        // use an intent to travel from one activity to another.
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
