2c1c69e82fde5b5ac8ff2bca71bc7bc6a317101e
thompluk <thomasdany38@gmail.com>
2022-09-23 03:21:22 +0700

    2

--- app/src/main/java/com/example/lat_ugd1/MenuActivity.kt
+++ app/src/main/java/com/example/lat_ugd1/MenuActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.example.lat_ugd1.room.UserDB
@@ -12,9 +13,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarItemView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MenuActivity : AppCompatActivity() {
+    val db by lazy { UserDB(this) }
     lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var name: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +29,15 @@ class MenuActivity : AppCompatActivity() {
         val fragment2 = FragmentProfile()
         val fragment3 = FragmentNotification()
 
+        name = findViewById(R.id.username)
+
         setCurrentFragment(fragment1)
         bottomNavigation = findViewById(R.id.bottom_navigation) as BottomNavigationView
         bottomNavigation.setOnNavigationItemReselectedListener {
             if (it.itemId == R.id.page_1) {
                 setCurrentFragment(fragment1)
             } else if (it.itemId == R.id.page_2) {
+                getUser()
                 setCurrentFragment(fragment2)
             } else {
                 setCurrentFragment(fragment3)
@@ -42,4 +51,12 @@ class MenuActivity : AppCompatActivity() {
             replace(R.id.nav_fragment,fragment)
             commit()
         }
+    fun getUser(){
+        //userId = intent.getIntExtra("intent_id", 0)
+        CoroutineScope(Dispatchers.IO).launch {
+            //sermentara id user 0 karena anggapan user hanya 1 saat ini
+            val users = db.userDao().getUser(0)[0]
+            name.setText(users.username)
+        }
+    }
 }
