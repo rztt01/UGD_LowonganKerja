ca114bdab67e86808f0be9eab40c41746f30aa96
thompluk <thomasdany38@gmail.com>
2022-09-23 17:11:56 +0700

    Update SplashScreen.kt

--- app/src/main/java/com/example/lat_ugd1/SplashScreen.kt
+++ app/src/main/java/com/example/lat_ugd1/SplashScreen.kt
@@ -12,32 +12,12 @@ import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
-    var sharedPreferences: SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        sharedPreferences = getSharedPreferences("pref", Context.MODE_PRIVATE)
-
-        if(!sharedPreferences!!.contains("status")){
-            setStatus("true")
-
-            Handler(Looper.getMainLooper()).postDelayed({
-                startActivity(Intent(this@SplashScreen, MainActivity::class.java))
-                finish()
-            },3000)
-        }
-
-        if(sharedPreferences!!.getString("status","")=="true"){
-            setStatus("false")
-        }else{
-            Handler(Looper.getMainLooper()).postDelayed({
-                startActivity(Intent(this@SplashScreen, MainActivity::class.java))
-                finish()
-            },3000)
-        }
-
-        /*window.setFlags(
+        window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
@@ -46,14 +26,7 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)*/
+        }, 3000)
     }
 
-    private fun setStatus(status:String){
-        sharedPreferences = getSharedPreferences("pref", Context.MODE_PRIVATE)
-        val editor: SharedPreferences.Editor = sharedPreferences!!.edit()
-
-        editor.putString("status", status)
-        editor.apply()
-    }
 }
