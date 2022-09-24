e0ae7e528efa166d330f815bc7a43c0c8999412a
rztt01* <vinsariest@gmail.com>
2022-09-24 15:41:49 +0700

    Notif Firebase

---
+++ app/src/main/java/com/example/lat_ugd1/FragmentNotes.kt
@@ -0,0 +1,11 @@
+package com.example.lat_ugd1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FragmentNotes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_notes)
+    }
+}
