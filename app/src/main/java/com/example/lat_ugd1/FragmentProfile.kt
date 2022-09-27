5182994bebd546fdd6c36fb0474eadc23a2e354b
rztt01* <vinsariest@gmail.com>
2022-09-27 20:04:43 +0700

    Puji Tuhan show profil done

--- app/src/main/java/com/example/lat_ugd1/FragmentProfile.kt
+++ app/src/main/java/com/example/lat_ugd1/FragmentProfile.kt
@@ -9,8 +9,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.lat_ugd1.databinding.FragmentProfileBinding
 import com.example.lat_ugd1.room.UserDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class FragmentProfile : Fragment(){
+
+    var iduser = "id_user"
+    var pref = "preference"
 
     val db by lazy {activity?.let { UserDB(it) }}
     private var _binding: FragmentProfileBinding? = null
@@ -21,35 +27,33 @@ class FragmentProfile : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        var iduser = "id_user"
-        var pref = "preference"
         var sharedPreferences : SharedPreferences? = null
 
         super.onViewCreated(view, savedInstanceState)
         sharedPreferences = activity?.getSharedPreferences(pref, Context.MODE_PRIVATE)
         val id = sharedPreferences!!.getInt(iduser, 0)
-        val prefe = sharedPreferences!!.getString(pref, "")
 
-//        var username = binding.root.tvUsername
-//        var email = binding.root.tvEmail
-//        var date = binding.root.tvDate
-//        var phone = binding.root.tvPhone
-//
-//
-//
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val user = db?.userDao()?.getUser(id)?.get(0)
-//            username.setText(user?.username)
-//            email.setText(user?.email)
-//            date.setText(user?.tanggalLahir)
-//            phone.setText(user?.noTelp)
-//        }
+           var username = binding.tvUsername
+            var email = binding.tvEmail
+            var date = binding.tvDate
+            var phone = binding.tvPhone
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val user = db?.userDao()?.getUser(id)?.get(0)
+            username.setText(user?.username)
+            email.setText(user?.email)
+            date.setText(user?.tanggalLahir)
+            phone.setText(user?.noTelp)
+        }
 
 
 
     }
 }
+
+
